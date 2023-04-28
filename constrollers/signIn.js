56350ce7cdd81e3e59f7e322409c74aa361036d0
Paul Adrian <paulreyes74@yahoo.com>
2023-04-28 16:00:08 -0500

    added condition for signin

--- constrollers/signIn.js
+++ constrollers/signIn.js
@@ -1,12 +1,16 @@
 const handleSignIn = (req, res, bcrypt, db) => {
+    const { email, password } = req.body
 
+    if (!email || !password) {
+        return res.status(400).json('Empty information has beebn submitted')
+    }
     db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
+        .where('email', '=', email)
         .then(data => {
-            const passValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            const passValid = bcrypt.compareSync(password, data[0].hash);
             if (passValid) {
                 return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
+                    .where('email', '=', email)
                     .then(user => {
                         res.json(user[0])
                     })
