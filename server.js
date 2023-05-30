e61e69052ff887d582ee658f60b64861b3243e68
Paul Adrian <paulreyes74@yahoo.com>
2023-05-30 15:33:52 -0500

    Updated back end app code with DB info

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ const image = require('./constrollers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'postgreadmin',
-        database: 'smartbrain',
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+        host: process.env.DATABASE_HOST,
+        port: 5432,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PW,
+        database: process.env.DATABASE_DB,
     }
 });
 
